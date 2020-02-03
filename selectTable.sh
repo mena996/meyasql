3993effe541e82b32cf5614799675836c4c32ab2
YahyaWsea <yahya.wsea@gmail.com>
2020-02-03 04:05:31 +0200

    selectTable added

---
+++ selectTable.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+
+echo -n "enter the name of table to show from ( $databaseName ) :  "
+read tblName
+cd ~/meyasql/$databaseName
+check=`ls | grep "^$tblName$"`
+
+if [ "$check" = "" ]; 
+then
+echo "this table is not found";
+
+elif [ "$check" = "$tblName" ]; 
+then
+echo
+awk -F: '{ print $0 }' $check;
+echo
+
+fi
+
+
