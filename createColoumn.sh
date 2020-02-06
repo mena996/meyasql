3be5931ff34592f09166e56010bb9a1bb18aa8a7
YahyaWsea <yahya.wsea@gmail.com>
2020-02-06 14:13:30 +0200

    creating table with changes in code

--- createColoumn.sh
+++ createColoumn.sh
@@ -24,40 +24,10 @@ do
             fi
 
             read colName
-
-            # check=`awk -F: '{ print $1 }' .metaData_$2 | grep "^$colName$"`
-
-            # if [ "$check" = "" ]; then
-
-            #     select choice in "string" "number" ;
-            #     do
-            #         case $REPLY in
-            #             1 ) dataType="string"
-            #                 break;;
-            #             2 ) dataType="number"
-            #                 break;;
-            #         esac
-            #     done
-
-            #     awk -F: ' { print $0 } END{ print "'$colName:$dataType'" }' .metaData_$2 > temp.tmp && mv temp.tmp .metaData_$2
-
-            #     check=`cat $2`
-            #     if [ "$check" = "" ]; then
-            #         awk -F: ' BEGIN{ print "'$colName'" } ' $2 > temp.tmp && mv temp.tmp $2
-            #     else
-            #         awk -F: ' { if (NR == 1) sub(/$/,"':$colName'") ; print $0 } ' $2 > temp.tmp && mv temp.tmp $2
-            #     fi
-
-
-            # else
-            #     echo "this column already exists";
-            # fi
-            #     . ../../CreateDetailTbl.sh
             ;;
 
         2 )
             . ./tableManager
             ;;
-
     esac
 done
